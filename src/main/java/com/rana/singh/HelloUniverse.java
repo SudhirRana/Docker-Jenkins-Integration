2180a819adb2df4f9f3c152e386e9df10d233c49
SudhirRana <sudhirrana500@gmail.com>
2022-02-07 18:39:01 +0530

    syste commit

--- src/main/java/com/rana/singh/HelloUniverse.java
+++ src/main/java/com/rana/singh/HelloUniverse.java
@@ -4,6 +4,7 @@ public class HelloUniverse {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		System.out.println("Are u angree");
+		System.out.println("Are u angree");
 		
 	}
 
